8a9e97af242b64b772b2507800cccad0159d4333
Shashini <it22349460@my.sliit.lk>
2026-04-05 18:57:44 +0530

    Complete Resources API module with CRUD and search

---
+++ backend/src/main/java/com/campus/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.campus.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
