74afcdf923ecc6f1e3b21d2558e5fe53f48ef4cf
Shashini12 <127285181+Shashini12@users.noreply.github.com>
2026-04-21 20:51:14 +0530

    Update ResourceRepository.java
    fix update resource bug

--- backend/src/main/java/com/smartcampus/repository/ResourceRepository.java
+++ backend/src/main/java/com/smartcampus/repository/ResourceRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+
 @Repository
 public interface ResourceRepository extends MongoRepository<Resource, String> {
     boolean existsByName(String name);
